cf2f5961260355c96825b9fbd51ef77973a38ed3
wbhaao <a31620504@gmail.com>
2022-04-10 14:25:32 +0900

    성공인데효율성실패

--- 등굣길.py
+++ 등굣길.py
@@ -1,22 +1,25 @@
 # 시작부터 잘못됐다!
+from collections import deque
+
 def solution(m, n, puddles):
-    lst = [[1, 1]]
+    lst = deque([[1, 1]])
+    cnt = 0
     
-    
-    while True:
+    while lst:
         # 앞에 물웅덩이와 벽이 없으면
-        for i in range()
-        if lst[0][0]+1 > m or \
-          [lst[0][0]+1, lst[0][1]] == puddles:
-            pass
+        if lst[0] == [m,n]:
+            cnt += 1
         else:
-            lst.append([lst[0][0]+1, lst[0][1]])
-        # 앞에 물웅덩이와 벽이 없으면
-        if lst[0][1]+1 > n or \
-          [lst[0][0], lst[0][1]+1] == puddles:
-            pass
-        else:
-            lst.append([lst[0][0], lst[0][1]]+1)
-        del lst[0]
-    answer = 0
-    return answer
+            # 앞에 물웅덩이와 벽이 없으면 (가로 전진)
+            if lst[0][0]+1 <= m and \
+               not ([lst[0][0]+1, lst[0][1]] in puddles):
+                lst.append([lst[0][0]+1, lst[0][1]])
+            # 앞에 물웅덩이와 벽이 없으면 (세로 전진)
+            if lst[0][1]+1 <= n and \
+               not ([lst[0][0], lst[0][1]+1] in puddles):
+                lst.append([lst[0][0], lst[0][1]+1])
+                
+        lst.popleft()
+    return cnt % 1000000007
+
+print(solution(4, 3, [[2, 2]]))
