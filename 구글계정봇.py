600d855faac93d3ede39da03fe724b51b0537092
wbhaao <a31620504@gmail.com>
2022-04-11 20:02:33 +0900

    전번차단돼따

---
+++ 구글계정봇.py
@@ -0,0 +1,116 @@
+import pyautogui
+import time
+import sys
+import random
+
+SLEEP_TIME = 2.2
+
+def find_target(img_file, timeout=30):
+    start = time.time()
+    target = None
+    while target is None:
+        target = pyautogui.locateOnScreen(img_file)
+        end = time.time()
+        if end - start > timeout:
+            break
+    return target
+
+def my_click(img_file, timeout=30):
+    target = find_target(img_file, timeout)
+    if target:
+        pyautogui.click(target)
+    else:
+        print(f"[Timeout {timeout}s] Target not found ({img_file}). Terminate program.")
+        sys.exit()
+import pyperclip
+
+def my_write(text):
+    pyperclip.copy(text)
+    time.sleep(SLEEP_TIME/3)
+    pyautogui.hotkey("ctrl", "v")
+
+def random_Name():
+    name = (random.randint(0, 15)*588)+(random.randint(0, 20)*28)+random.randint(0, 27)+44032
+    return chr(name)
+
+# 크롬 선택
+my_click("chrome.png", 10)
+time.sleep(SLEEP_TIME*1.5)
+# 링크 입력
+my_write("https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp")
+pyautogui.hotkey("enter")
+time.sleep(SLEEP_TIME*2.2)
+# 성 입력
+my_write(random_Name())
+pyautogui.hotkey("enter")
+time.sleep(SLEEP_TIME)
+# 이름 입력
+my_write(random_Name())
+pyautogui.hotkey("enter")
+time.sleep(SLEEP_TIME)
+# 아이디 입력
+my_write("glistABC2")
+pyautogui.hotkey("enter")
+time.sleep(SLEEP_TIME)
+# 비번 입력
+my_write("goo!1052")
+pyautogui.hotkey("enter")
+time.sleep(SLEEP_TIME)
+# 비번 재확인
+my_write("goo!1052")
+pyautogui.hotkey("enter")
+time.sleep(SLEEP_TIME)
+
+# 확인
+pyautogui.moveTo(493, 542)
+pyautogui.click()
+time.sleep(SLEEP_TIME*1.5)
+# 전번 찍기
+my_write("01031620504")
+pyautogui.hotkey("enter")
+time.sleep(SLEEP_TIME*10)
+
+# 여기에 확인문자 받는 텀이 있어야함
+
+# 복구 이메일
+pyautogui.moveTo(440, 520)
+pyautogui.click()
+my_write("a31620504@gmail.com")
+time.sleep(SLEEP_TIME)
+# 년도
+pyautogui.moveTo(365,605)
+pyautogui.click()
+my_write("2000")
+time.sleep(SLEEP_TIME)
+# 월
+pyautogui.moveTo(477, 598)
+pyautogui.click()
+time.sleep(SLEEP_TIME)
+my_click("May.png", 10)
+time.sleep(SLEEP_TIME)
+# 일
+pyautogui.moveTo(603, 606)
+pyautogui.click()
+my_write("1")
+time.sleep(SLEEP_TIME)
+# 성별
+pyautogui.moveTo(462,690)
+pyautogui.click()
+time.sleep(SLEEP_TIME)
+my_click("male.png", 10)
+time.sleep(SLEEP_TIME*1.5)
+
+my_click("next.png", 10)
+time.sleep(SLEEP_TIME*1.5)
+
+my_click("skip.png", 10)
+time.sleep(SLEEP_TIME*1.5)
+
+pyautogui.moveTo(1273,268)
+pyautogui.dragTo(1271,947)
+time.sleep(SLEEP_TIME)
+my_click("checkBox.png", 10)
+time.sleep(SLEEP_TIME)
+my_click("checkBox.png", 10)
+time.sleep(SLEEP_TIME)
+my_click("createAccount.png", 10)
--- 등굣길.py
+++ 등굣길.py
@@ -1,4 +1,5 @@
 # 시작부터 잘못됐다!
+# 지금 해석할거
 from collections import deque
 
 def solution(m, n, puddles):
@@ -23,3 +24,20 @@ def solution(m, n, puddles):
     return cnt % 1000000007
 
 print(solution(4, 3, [[2, 2]]))
+
+# 해석용
+def solution(m, n, puddles):
+    # 왜 거꾸로 할까..
+    puddles = [[q,p] for [p,q] in puddles]      # 미리 puddles 좌표 거꾸로
+    # dp는 
+    dp = [[0] * (m + 1) for _ in range(n + 1)]  
+    dp[1][1] = 1          
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1: continue
+            if [i, j] in puddles:    # 웅덩이 위치의 경우 값을 0으로
+                dp[i][j] = 0
+            else:                    # 현재 칸은 왼쪽 칸, 위 칸의 합산!
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+    return dp[n][m]
