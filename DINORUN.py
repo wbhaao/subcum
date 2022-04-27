63e7df11d19a945911b3de8c27abdd4ee884c2a2
wbhaao <a31620504@gmail.com>
2022-04-27 20:02:37 +0900

    구명보트 풀이

--- DINORUN.py
+++ DINORUN.py
@@ -1,38 +1,66 @@
-from time import *
-from keyboard import *
+# from time import *
+# from keyboard import *
 
-# 0:하늘
-# 1:땅
-# 2:공룡
-# 3:선인장
-# 4:구름
-blockList = ["🟦","🟩","🟨","🟥","⬜️"]
+# # 0:하늘
+# # 1:땅
+# # 2:공룡
+# # 3:선인장
+# # 4:구름
+# blockList = ["🟦","🟩","🟨","🟥","⬜️"]
 
-display = [[0,0,0,0,0, 0,0,0,0,0],
-           [0,0,0,0,0, 0,0,0,0,0],
-           [0,0,2,0,0, 0,0,0,0,0],
-           [0,0,2,0,0, 0,0,0,0,0],
-           [1,1,1,1,1, 1,1,1,1,1],]
+# display = [[0,0,0,0,0, 0,0,0,0,0],
+#            [0,0,0,0,0, 0,0,0,0,0],
+#            [0,0,2,0,0, 0,0,0,0,0],
+#            [0,0,2,0,0, 0,0,0,0,0],
+#            [1,1,1,1,1, 1,1,1,1,1],]
 
-def printDisplay():
-    for dis in display:
-        for d in dis:
-            print(blockList[d], end="")
-        print()
+# def printDisplay():
+#     for dis in display:
+#         for d in dis:
+#             print(blockList[d], end="")
+#         print()
 
-while True:
-    if read_key() == "space":
-        print('hlelo')
-        break
-    # 선인장이 없으면
-    if not any(3 in l for l in display):
-        display[3][9] = 3
-    # 선인장이 있으면
-    else:
-        cactusIndex = display[3].index(3)
-        display[3][cactusIndex] = 0
-        # 넘으면 추가하지 않고 삭제만 함(그러면 위에 if에 걸림)
-        if cactusIndex-1 > -1:
-            display[3][cactusIndex-1] = 3
-    printDisplay()
-    sleep(0.5)
+# while True:
+#     if read_key() == "space":
+#         print('hlelo')
+#         break
+#     # 선인장이 없으면
+#     if not any(3 in l for l in display):
+#         display[3][9] = 3
+#     # 선인장이 있으면
+#     else:
+#         cactusIndex = display[3].index(3)
+#         display[3][cactusIndex] = 0
+#         # 넘으면 추가하지 않고 삭제만 함(그러면 위에 if에 걸림)
+#         if cactusIndex-1 > -1:
+#             display[3][cactusIndex-1] = 3
+#     printDisplay()
+#     sleep(0.5)
+
+def solution(s):
+    lst = list(map(int, s.split(" ")))
+    return "%d %d" % (min(lst), max(lst))
+
+def solution(priorities, location):
+    cnt = 1
+    priorities_dict = {} # temp는 무슨역할?
+
+    for index, p in enumerate(priorities): # 인덱스를 지정해줘서 loca와 index가 같으면 return
+        priorities_dict[index] = p 
+    # key : 인덱스 , value : 값, priorities : 값
+    while True:
+        # 첫번째가 priorities_dict 중 우선도가 가장 높나?
+        if next(iter(priorities_dict.values())) == max(priorities_dict.values()): 
+            # 나의 인덱스와 구하고자 하는 index가 같나
+            if next(iter(priorities_dict.keys())) == location:
+                return cnt
+            # 다르면 삭제
+            del priorities_dict[next(iter(priorities_dict.keys()))] 
+            cnt += 1 
+        else: # 해석 : 맨앞에껄 맨뒤로 옮기는 것
+            # key와 value를 저장하고 마지막에 index에 append
+            temp_key = next(iter(priorities_dict)) # 삭제하는 key 저장
+            temp_value = priorities_dict[next(iter(priorities_dict))] # 삭제하는 value 저장
+            del priorities_dict[next(iter(priorities_dict.keys()))] # 첫번째 순서 삭제
+            # 첫번쨰 값 append
+            priorities_dict[temp_key] = temp_value    
--- programmers/test.py
+++ programmers/test.py
@@ -49,15 +49,49 @@ lock = [0]
 # and
 #lock에 0이 존재하지 않는지?
 # 하나라도 False면 False
-if all(0 != l for l in lock):
-    # return True
-    pass
-# or
-#lock에 0이 하나라도 존재하는지?
-# 하나라도 True면 True
-if any(0 == l for l in lock):
-        #return True
-    pass
+# if all(0 != l for l in lock):
+#     # return True
+#     pass
+# # or
+# #lock에 0이 하나라도 존재하는지?
+# # 하나라도 True면 True
+# if any(0 == l for l in lock):
+#         #return True
+#     pass
 
-
-
+def solution(people, limit):
+    cnt = 0
+    people.sort()
+    while len(people) > 1:
+        cnt += 1
+        # 내보낼 수 있다
+        if people[0]+people[-1] <= limit:
+            del people[0]
+            del people[-1]
+        else:
+            del people[-1]
+    answer = len(people) + cnt
+    return answer
+print(solution([70, 50, 80, 50], 100))
+########################################
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    # f:front
+    # e:end
+    f_cnt =0
+    e_cnt =len(people)-1 # index니까 -1
+    while e_cnt - f_cnt >= 1: # 같은 수 => 탈출
+        # 가장 작은 수 + 가장 큰 수 <= limit
+        if people[f_cnt] + people[e_cnt] <= limit:
+            # f와 end 전부 삭제 처리 (좌표 앞으로)
+            f_cnt += 1
+            e_cnt -= 1
+        else:
+            # 아니면 뒷수만 삭제 처리
+            e_cnt -= 1
+        answer += 1
+    # 수 하나가 남았을 경우 (없을 경우 f와 e가 교차됨)
+    if e_cnt == f_cnt:
+        answer += 1
+    return answer
